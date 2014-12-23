fed24cc843a5bd7142126243b2fc832bc7b805f5
FG Ribreau <github@fgribreau.com>
2014-12-23 22:37:37 +0100

    .

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-common-env
+common-env [![Build Status](https://drone.io/github.com/FGRibreau/common-env/status.png)](https://drone.io/github.com/FGRibreau/common-env/latest)
+[![Deps](https://david-dm.org/FGRibreau/common-env.png)](https://david-dm.org/FGRibreau/common-env)
+[![Version](http://badge.fury.io/js/common-env.png)](https://david-dm.org/FGRibreau/common-env)
+
 ==========
 
-A little helper I use everywhere
+A little helper I use everywhere for configuration. Environment variables are a really great way to quickly change a program behavior.
+
+<p align="center">
+<img style="width:100%" src="./docs/configuration.gif"/>
+</p>
+
+# npm
+
+```shell
+npm install common-env
+```
+
+# Usage
+
+```javascript
+var env = require('common-env');
+
+// AMQP_CONNECT=true node test.js
+var config = env.getOrElseAll({
+  amqp: {
+    login: 'guest',
+    password: 'guest',
+    host: 'localhost',
+    port: 5672,
+    connect: false
+  },
+
+  FULL_UPPER_CASE: {
+    PORT: 8080
+  },
+
+  MICROSTATS: {
+    HASHKEY: 'B:mx:global'
+  }
+});
+
+t.strictEqual(config.amqp.login, 'plop'); // converted from env
+t.strictEqual(config.amqp.port, 5672);
+t.strictEqual(config.amqp.connect, true); // converted from env
+t.strictEqual(config.FULL_UPPER_CASE.PORT, 8080);
+```
---
+++ test/env.test.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var envFactory = require('..');
+var t = require('chai').assert;
+var _ = require('lodash');
+
+describe('env', function () {
+  var env, logger;
+
+  beforeEach(function () {
+    logger = {
+      calls: [],
+      info: function () {
+        this.calls.push(_.toArray(arguments).join(' '));
+        console.log.apply(console, arguments);
+      },
+      hasENV: function (env_var) {
+        return this.calls.some(function (line) {
+          return _.contains(line, env_var);
+        });
+      }
+    };
+    env = envFactory(logger);
+    process.env.AMQP_LOGIN = 'plop';
+    process.env.AMQP_CONNECT = 'tRue';
+    process.env.AMQP_CONNECT2 = 'false';
+  });
+
+  describe('.getOrElseAll', function () {
+
+    it('should return an object', function () {
+      var config = env.getOrElseAll({
+        AMQP: {
+          LoGiN: 'guest', // add a bug inside key name (mix lower/upper case)
+          PASSWORD: 'guest',
+          HOST: 'localhost',
+          PORT: 5672,
+          connect: false,
+          connect2: true,
+          PLOP: {
+            ok: {
+              heyheyhey: true
+            }
+          }
+        },
+
+        c: {
+          PORT: 8080,
+          root: ''
+        },
+
+        MICROSTATS: {
+          HASHKEY: 'B:mx:global'
+        }
+      });
+
+      t.strictEqual(config.AMQP.LoGiN, 'plop');
+      t.strictEqual(config.AMQP.PORT, 5672);
+      t.strictEqual(config.AMQP.PLOP.ok.heyheyhey, true);
+      t.strictEqual(config.AMQP.connect, true);
+      t.strictEqual(config.AMQP.connect2, false);
+      t.strictEqual(config.c.root, '');
+    });
+
+    it('should return ask for ENV vars', function () {
+      env.getOrElseAll({
+        plop: {
+          root_token: 'sdfopiqjsdfpoij',
+          api: {
+            endpoint_protocol: 'https',
+            endpoint_host: 'sqdfqsdf.cleverapps.io',
+            endpoint_port: ''
+          },
+          strategy: 'https://strategy.plop.net',
+        }
+      });
+
+      t.ok(logger.hasENV('PLOP_ROOT_TOKEN'), 'PLOP_ROOT_TOKEN');
+      t.ok(logger.hasENV('PLOP_API_ENDPOINT_PORT'), 'PLOP_API_ENDPOINT_PORT');
+    });
+  });
+
+  afterEach(function () {
+    delete process.env.AMQP_LOGIN;
+  });
+
+});
